7de178ee37cc2a445ad6cdecd918fb57dfb9089e
Pavel Khokhlov <khokhlovpavel1986@yandex.ru>
2022-03-02 23:21:31 +0300

    ! code for npm package

--- bin/pageLoader.js
+++ bin/pageLoader.js
@@ -6,8 +6,9 @@ import pageLoader from '../src/index.js';
 program
   .version('1.0.0')
   .description('Page download')
-  .option('-o, --output', 'output directory', '.')
-  .arguments('<url> [dirPath]')
+  .option('-o, --output', 'output directory', '/app')
+  .argument('<url>')
+  .argument('[dirPath]', '', '/app')
   .action(async (url, dirPath) => {
     console.log(await pageLoader(url, dirPath));
   })
--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import path from 'path';
 import axios from 'axios';
 import convertUrl from './utils/convertUrl.js';
 
-export default async (url, dirPath = '.') => {
+export default async (url, dirPath) => {
   try {
     const response = await axios.get(url);
+    const formattedDirPath = `.${dirPath}`;
     if (response.status === 200) {
-      if (!fs.existsSync(dirPath)) {
-        await fs.promises.mkdir(dirPath);
+      if (!fs.existsSync(formattedDirPath)) {
+        await fs.promises.mkdir(formattedDirPath);
       }
       const fileName = `${convertUrl(url)}.html`;
-      const resultPath = path.join(dirPath, fileName);
+      const resultPath = path.join(formattedDirPath, fileName);
       await fs.promises.writeFile(resultPath, response.data);
       return resultPath;
     }
