d2e127b94b93117c63df6250e48ccad0a031050e
Pavel Khokhlov <khokhlovpavel1986@yandex.ru>
2022-03-07 13:14:36 +0300

    + tests for download links & scripts

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,8 +4,57 @@ import fs from 'fs';
 import nock from 'nock';
 import cheerio from 'cheerio';
 import pageLoader from '../src/index.js';
+import isValidHttpUrl from '../src/utils/isValidHttpUrl.js';
+import getOriginFromUrl from '../src/utils/getOriginFromUrl.js';
+import getPathFromUrl from '../src/utils/getPathFromUrl.js';
 
 const getFixturePath = (name) => path.join('__tests__', '__fixtures__', name);
+const getScopes = (sources, baseUrl) => {
+  sources.forEach((src) => {
+    if (!isValidHttpUrl(src)) {
+      nock(baseUrl)
+        .get(src)
+        .reply(200);
+    }
+    if (isValidHttpUrl(src) && getOriginFromUrl(src) === baseUrl) {
+      nock(baseUrl)
+        .get(getPathFromUrl(src))
+        .reply(200);
+    }
+  });
+};
+const setExpectedSourcesData = (file, selector, attr, arraySources, arrayFilenames, baseUrl) => {
+  const $ = cheerio.load(file);
+  $(selector).each((function (i) {
+    const source = $(this).attr(attr);
+    if (!isValidHttpUrl(source)
+      || (isValidHttpUrl(source) && getOriginFromUrl(source) === baseUrl)) {
+      // eslint-disable-next-line no-param-reassign
+      arraySources[i] = source;
+      // eslint-disable-next-line prefer-destructuring,no-param-reassign
+      arrayFilenames[i] = source.split('/')[1];
+    }
+  }));
+};
+
+const setResponseSourcesData = (file, selector, arraySources, attr) => {
+  const $ = cheerio.load(file);
+  $(selector).each((function (i) {
+    // eslint-disable-next-line no-param-reassign
+    arraySources[i] = $(this).attr(attr);
+  }));
+};
+
+const setActualSourcesData = (file, selector, attr, arraySources) => {
+  const $ = cheerio.load(file);
+  $(selector).each((function (i) {
+    const source = $(this).attr(attr);
+    if (!isValidHttpUrl(source)) {
+      // eslint-disable-next-line no-param-reassign
+      arraySources[i] = source;
+    }
+  }));
+};
 
 const baseUrl = 'https://ru.hexlet.io';
 const uri = '/courses';
@@ -15,36 +64,39 @@ const savedFileName = 'savedPage.html';
 
 let dirPath = '';
 let url = '';
-let expectedFilePath = '';
+let expectedHtmlFilePath = '';
 let expectedFile;
-const expectedImageFilesSources = [];
+const expectedImagesSources = [];
+const expectedLinksSources = [];
+const expectedScriptsSources = [];
+
 const expectedNamesImageFiles = [];
+const expectedNamesLinkFiles = [];
+const expectedNamesScriptFiles = [];
 
 let responseFile;
-const responseImageFilesSources = [];
+const responseImagesSources = [];
+const responseLinksSources = [];
+const responseScriptsSources = [];
 
 beforeAll(async () => {
   url = `${baseUrl}${uri}`;
   responseFile = await fs.promises.readFile(getFixturePath(responseFileName), 'utf-8');
   expectedFile = await fs.promises.readFile(getFixturePath(savedFileName), 'utf-8');
 
-  const $expectedPage = cheerio.load(expectedFile);
-  $expectedPage('img').each((function (i) {
-    expectedImageFilesSources[i] = $expectedPage(this).attr('src');
-    // eslint-disable-next-line prefer-destructuring
-    expectedNamesImageFiles[i] = $expectedPage(this).attr('src').split('/')[1];
-  }));
+  setExpectedSourcesData(expectedFile, 'img', 'src', expectedImagesSources, expectedNamesImageFiles, baseUrl);
+  setExpectedSourcesData(expectedFile, 'link', 'href', expectedLinksSources, expectedNamesLinkFiles, baseUrl);
+  setExpectedSourcesData(expectedFile, 'script', 'src', expectedScriptsSources, expectedNamesScriptFiles, baseUrl);
 
-  const $responsePage = cheerio.load(responseFile);
-  $responsePage('img').each((function (i) {
-    responseImageFilesSources[i] = $responsePage(this).attr('src');
-  }));
+  setResponseSourcesData(responseFile, 'img', responseImagesSources, 'src');
+  setResponseSourcesData(responseFile, 'link', responseLinksSources, 'href');
+  setResponseSourcesData(responseFile, 'script', responseScriptsSources, 'src');
 });
 
 beforeEach(async () => {
   dirPath = await fs.promises.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-  expectedFilePath = path.join(dirPath, expectedFilename);
-  const expectedFilesPath = `${expectedFilePath.slice(0, -5)}_files`;
+  expectedHtmlFilePath = path.join(dirPath, expectedFilename);
+  const expectedFilesPath = `${expectedHtmlFilePath.slice(0, -5)}_files`;
   await fs.promises.mkdir(expectedFilesPath);
 });
 
@@ -55,31 +107,44 @@ test('checkFileName', async () => {
   await pageLoader(url, dirPath);
   const actualFiles = await fs.promises.readdir(dirPath);
   const actualHtmlFile = actualFiles.find((file) => file.match(/.html$/));
-  const actualHtmlPath = path.join(dirPath, actualHtmlFile);
-  expect(actualHtmlPath).toEqual(expectedFilePath);
+  const actualHtmlFilePath = path.join(dirPath, actualHtmlFile);
+  expect(actualHtmlFilePath).toEqual(expectedHtmlFilePath);
 });
 
-test('checkDownloadedImages', async () => {
+test('checkDownloadedFiles', async () => {
   nock(baseUrl)
     .get(uri)
     .reply(200, responseFile);
-  responseImageFilesSources.forEach((src) => {
-    nock(baseUrl)
-      .get(src)
-      .reply(200);
-  });
+
+  getScopes(responseImagesSources, baseUrl);
+  getScopes(responseLinksSources, baseUrl);
+  getScopes(responseScriptsSources, baseUrl);
+
   await pageLoader(url, dirPath);
+
   const actualFiles = await fs.promises.readdir(dirPath);
   const actualHtmlFile = actualFiles.find((file) => file.match(/.html$/));
-  const actualHtmlPath = path.join(dirPath, actualHtmlFile);
-  const actualDirPathOfFiles = `${actualHtmlPath.slice(0, -5)}_files`;
-  const actualNamesImageFiles = await fs.promises.readdir(actualDirPathOfFiles);
-  const actualHtmlContent = await fs.promises.readFile(actualHtmlPath, { encoding: 'utf-8' });
-  const $actualPage = cheerio.load(actualHtmlContent);
-  const actualImageFilesSources = [];
-  $actualPage('img').each((function (i) {
-    actualImageFilesSources[i] = $actualPage(this).attr('src');
-  }));
-  expect(actualImageFilesSources).toEqual(expectedImageFilesSources);
+  const actualHtmlFilePath = path.join(dirPath, actualHtmlFile);
+  const actualDirPathOfFiles = `${actualHtmlFilePath.slice(0, -5)}_files`;
+  const actualNamesFiles = await fs.promises.readdir(actualDirPathOfFiles);
+  const actualNamesImageFiles = actualNamesFiles.filter((file) => file.slice(-3) === 'png' || 'jpg');
+  const actualNamesLinkFiles = actualNamesFiles.filter((file) => file.slice(-3) === 'css');
+  const actualNamesScriptFiles = actualNamesFiles.filter((file) => file.slice(-2) === 'js');
+  const actualHtmlContent = await fs.promises.readFile(actualHtmlFilePath, { encoding: 'utf-8' });
+
+  const actualImagesSources = [];
+  const actualLinksSources = [];
+  const actualScriptSources = [];
+
+  setActualSourcesData(actualHtmlContent, 'img', 'src', actualImagesSources);
+  setActualSourcesData(actualHtmlContent, 'link', 'href', actualLinksSources);
+  setActualSourcesData(actualHtmlContent, 'script', 'src', actualScriptSources);
+
+  expect(actualImagesSources).toEqual(expectedImagesSources);
+  expect(actualLinksSources).toEqual(expectedLinksSources);
+  expect(actualScriptSources).toEqual(expectedScriptsSources);
+
   expect(actualNamesImageFiles).toEqual(expectedNamesImageFiles);
+  expect(actualNamesLinkFiles).toEqual(expectedNamesLinkFiles);
+  expect(actualNamesScriptFiles).toEqual(expectedNamesScriptFiles);
 });
