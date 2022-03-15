8958a33a350ee2934ece2049f8ebf2818637ff41
Pavel Khokhlov <khokhlovpavel1986@yandex.ru>
2022-03-15 14:15:21 +0300

    + return

--- bin/pageLoader.js
+++ bin/pageLoader.js
@@ -9,11 +9,10 @@ program
   .arguments('<url>')
   .option('-o, --output [dirPath]', 'output directory', process.cwd())
   .action((url) => {
-    // console.log(program);
     console.log('url in bin', url);
     console.log('path in bin', program.getOptionValue('output'));
     console.log('program', program);
-    pageLoader(url, program.getOptionValue('output'))
+    return pageLoader(url, program.getOptionValue('output'))
       .then((outputDir) => console.log(outputDir))
       .catch(({ message }) => {
         console.error(message);
