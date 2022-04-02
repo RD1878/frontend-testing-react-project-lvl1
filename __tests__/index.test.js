91e5682b61639cae425dd0cf28ace73d5cbd7c2d
Pavel Khokhlov <khokhlovpavel1986@yandex.ru>
2022-04-02 16:46:44 +0300

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,116 +6,116 @@ import pageLoader from '../src/index.js';
 
 const getFixturePath = (name) => path.join(__dirname, '__fixtures__', name);
 const getFlattenHtml = (htmlValue) => htmlValue.replace(/\s*/g, '');
+const getFileValue = async (dirPath, filename) => {
+  const res = await fs.promises.readFile(path.join(dirPath, filename), 'utf-8');
+  return res;
+};
 
 const baseUrl = 'https://ru.hexlet.io';
+const uri = '/courses';
+const expectedHtmlFileName = 'ru-hexlet-io-courses.html';
 const responseHtmlFileName = 'responsePage.html';
 const savedHtmlFileName = 'savedPage.html';
 const scope = nock(baseUrl).persist();
 const errorNumbers = [404, 500];
 
-const resourcesData = {
-  html: {
-    uri: '/courses',
-    fileName: '',
-    expectedFileName: 'ru-hexlet-io-courses.html',
-    path: '/courses',
-  },
-  linkCss: {
+const resourcesData = [
+  {
+    name: 'linkCss',
     uri: '/assets',
     fileName: 'application.css',
     expectedFileName: 'ru-hexlet-io-assets-application.css',
-    path: '/assets/application.css',
+    pathFile: '/assets/application.css',
   },
-  linkHtml: {
+  {
+    name: 'linkHtml',
     uri: '/courses',
-    fileName: responseHtmlFileName,
+    fileName: 'responsePage.html',
     expectedFileName: 'ru-hexlet-io-courses.html',
-    path: '/courses',
+    pathFile: '/courses',
   },
-  image: {
+  {
+    name: 'image',
     uri: '/assets/professions',
     fileName: 'nodejs.png',
     expectedFileName: 'ru-hexlet-io-assets-professions-nodejs.png',
-    path: '/assets/professions/nodejs.png',
+    pathFile: '/assets/professions/nodejs.png',
   },
-  script: {
+  {
+    name: 'script',
     uri: '/packs/js',
     fileName: 'runtime.js',
     expectedFileName: 'ru-hexlet-io-packs-js-runtime.js',
-    path: '/packs/js/runtime.js',
+    pathFile: '/packs/js/runtime.js',
   },
-};
+];
 
 let dirPath = '';
 let url = '';
 let expectedHtmlFilePath = '';
 let expectedFile;
 let responseFile;
-let imageFile;
-let linkCssFile;
-let linkHtmlFile;
-let scriptFile;
-let expectedSourcesFilesValues;
 
 beforeAll(async () => {
-  url = `${baseUrl}${resourcesData.html.uri}`;
-  responseFile = await fs.promises.readFile(getFixturePath(responseHtmlFileName), 'utf-8');
-  expectedFile = await fs.promises.readFile(getFixturePath(savedHtmlFileName), 'utf-8').then((res) => getFlattenHtml(res));
-  imageFile = await fs.promises.readFile(getFixturePath(resourcesData.image.fileName), 'utf-8');
-  linkCssFile = await fs.promises.readFile(getFixturePath(resourcesData.linkCss.fileName), 'utf-8');
-  linkHtmlFile = await fs.promises.readFile(getFixturePath(resourcesData.linkHtml.fileName), 'utf-8');
-  scriptFile = await fs.promises.readFile(getFixturePath(resourcesData.script.fileName), 'utf-8');
-  expectedSourcesFilesValues = [linkCssFile, imageFile, linkHtmlFile, scriptFile];
+  url = `${baseUrl}${uri}`;
+  responseFile = await getFileValue(path.join(__dirname, '__fixtures__'), responseHtmlFileName);
+  expectedFile = await getFileValue(path.join(__dirname, '__fixtures__'), savedHtmlFileName).then((res) => getFlattenHtml(res));
 });
 
 beforeEach(async () => {
   dirPath = await fs.promises.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-  expectedHtmlFilePath = path.join(dirPath, resourcesData.html.expectedFileName);
-  // console.log(expectedHtmlFilePath);
+  expectedHtmlFilePath = path.join(dirPath, expectedHtmlFileName);
   const expectedFilesPath = `${expectedHtmlFilePath.slice(0, -5)}_files`;
   await fs.promises.mkdir(expectedFilesPath);
 });
 
-test('check files', async () => {
-  nock(baseUrl)
-    .get(resourcesData.html.uri)
-    .reply(200, responseFile)
-    .get(resourcesData.image.path)
-    .reply(200, imageFile)
-    .get(resourcesData.linkCss.path)
-    .reply(200, linkCssFile)
-    .get(resourcesData.linkHtml.path)
-    .reply(200, linkHtmlFile)
-    .get(resourcesData.script.path)
-    .reply(200, scriptFile);
-  await pageLoader(url, dirPath);
-  const actualFiles = await fs.promises.readdir(dirPath);
-  const actualHtmlFile = actualFiles.find((file) => file.match(/.html$/));
-  const actualSourcesDir = actualFiles.find((file) => file.match(/files$/));
-  const actualHtmlFilePath = path.join(dirPath, actualHtmlFile);
-  const actualSourcesDirPath = path.join(dirPath, actualSourcesDir);
-  const actualHtmlFileValue = await fs.promises.readFile(actualHtmlFilePath, 'utf-8');
-  const actualFlattenHtmlValue = getFlattenHtml(actualHtmlFileValue);
-  const actualSourcesFiles = await fs.promises.readdir(actualSourcesDirPath);
-  const actualSourcesFilesValues = await Promise.all(actualSourcesFiles.map((file) => fs.promises.readFile(path.join(actualSourcesDirPath, file), 'utf-8')));
-  await expect(actualSourcesFilesValues).toEqual(expectedSourcesFilesValues);
-  await expect(actualHtmlFilePath).toEqual(expectedHtmlFilePath);
-  await expect(actualFlattenHtmlValue).toEqual(expectedFile);
+describe('positive tests', () => {
+  test('check files', async () => {
+    scope
+      .get(uri)
+      .reply(200, responseFile);
+    await pageLoader(url, dirPath);
+    const actualFiles = await fs.promises.readdir(dirPath);
+    const actualHtmlFile = actualFiles.find((file) => file.match(/.html$/));
+    const actualHtmlFilePath = path.join(dirPath, actualHtmlFile);
+    const actualHtmlValue = getFlattenHtml(await fs.promises.readFile(actualHtmlFilePath, 'utf-8'));
+    await expect(actualHtmlFilePath).toEqual(expectedHtmlFilePath);
+    await expect(actualHtmlValue).toEqual(expectedFile);
+  });
+
+  test.each(resourcesData)('check sources values', async ({ fileName, expectedFileName }) => {
+    const index = resourcesData.findIndex((item) => item.fileName === fileName);
+    const value = await getFileValue(path.join(__dirname, '__fixtures__'), fileName);
+    scope
+      .get(uri)
+      .reply(200, responseFile)
+      .get(resourcesData[index].pathFile)
+      .reply(200, value);
+    await pageLoader(url, dirPath);
+    const actualFiles = await fs.promises.readdir(dirPath);
+    const actualSourcesDir = actualFiles.find((file) => file.match(/files$/));
+    const actualSourcesDirPath = path.join(dirPath, actualSourcesDir);
+    const actualValue = await getFileValue(actualSourcesDirPath, expectedFileName);
+    const expectedValue = await getFileValue(path.join(__dirname, '__fixtures__'), fileName);
+    await expect(actualValue).toEqual(expectedValue);
+  });
 });
 
-test.each(errorNumbers)('check with 404/500 error', async (code) => {
-  nock(baseUrl).get(`/${code}`).reply(code);
-  const failUrl = new URL(`/${code}`, baseUrl).toString();
-  await expect(pageLoader(failUrl, dirPath))
-    .rejects.toThrow(new RegExp(code));
-});
+describe('negative tests', () => {
+  test.each(errorNumbers)('check with 404/500 error', async (code) => {
+    nock(baseUrl).get(`/${code}`).reply(code);
+    const failUrl = new URL(`/${code}`, baseUrl).toString();
+    await expect(pageLoader(failUrl, dirPath))
+      .rejects.toThrow(new RegExp(code));
+  });
 
-test('check with file system error', async () => {
-  const failDir = '/fail_Dir';
-  const file = getFixturePath('nodejs.png');
-  scope
-    .get(resourcesData.html.uri)
-    .reply(200);
-  await expect(pageLoader(url, failDir)).rejects.toThrow('ENOENT');
-  await expect(pageLoader(url, file)).rejects.toThrow('ENOTDIR');
+  test('check with file system error', async () => {
+    const failDir = '/fail_Dir';
+    const file = getFixturePath('nodejs.png');
+    scope
+      .get(uri)
+      .reply(200);
+    await expect(pageLoader(url, failDir)).rejects.toThrow('ENOENT');
+    await expect(pageLoader(url, file)).rejects.toThrow('ENOTDIR');
+  });
 });
